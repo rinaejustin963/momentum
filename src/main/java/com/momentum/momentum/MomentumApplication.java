bcdb53a6662bdbbeac3385f223392ae24a87aecb
Rinae Justin Tshivhinda <justinrinae@gmail.com>
2023-06-06 00:17:37 +0200

    Update dependecies in Pom.xml

--- src/main/java/com/momentum/momentum/MomentumApplication.java
+++ src/main/java/com/momentum/momentum/MomentumApplication.java
@@ -1,9 +1,13 @@
 package com.momentum.momentum;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Investor-API", version = "1.0", description = "Springboot Rest API"))
 public class MomentumApplication {
 
 	public static void main(String[] args) {
